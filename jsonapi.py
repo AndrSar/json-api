afb49a20588fe25c49f7d31b21f4c394c1861f9b
Andrey Lifanov <andr-sar@yandex.ru>
2017-04-03 19:38:52 +0200

    work in progress

--- jsonapi.py
+++ jsonapi.py
@@ -11,70 +11,93 @@ class Specification:
             sig = inspect.signature(method)
             self._methods[method.__name__] = (sig, method)
 
-    def invoke_method_from_json_request(self, json_request) -> Response:
+    def invoke_method(self, dictionary):
         requested_method = None
         requested_args = None
 
         try:
             try:
-                requested_method = json_request['method']
+                requested_method = dictionary['method']
             except KeyError:
                 raise MethodIsNotSpecifiedError()
 
             try:
-                requested_args = json_request['args']
+                requested_args = dictionary['args']
             except KeyError:
                 raise MethodArgumentsAreNotSpecifiedError()
 
             try:
                 sig, method = self._methods[requested_method]
+                self.check_method_arguments(sig, requested_args)
+                result = method(**requested_args)
+                return Result('OK', result)
+
             except KeyError:
                 raise NoSuchMethodError(requested_method)
-        
+
         except Error as e:
             return e.response
 
 
-    def check_method_arguments(self, sig, method, args):
-        try:
-            for param in sig.parameters.values():
-                if param.default == param.empty:
-                    if (param.name in args)
+    def check_method_arguments(self, sig, args):
+        for param in sig.parameters.values():
+            try:
+                arg = args[param.name]
+                if param.annotation != param.empty:
+                    if param.annotation is not type(arg):
+                        received_type = type(arg).__name__
+                        expected_type = param.annotation.__name__
+                        raise InvalidArgumentTypeError(param.name, received_type, expected_type)
+            except KeyError:
+                raise RequiredArgumentIsMissedError(param.name)
 
 
 
-class Response:
-    def __init__(self, status: int, description: str, result=None):
-        self._status = status
+class Result:
+    def __init__(self, description: str, result=None, exception_type=None):
         self._description = description
         self._result = result
+        self._exception_type = exception_type
 
-    
-
-    def to_json_str(self):
+    def to_dict(self):
         return {
-            'status': self._status,
+            'result': self._result,
             'description': self._description,
-            'result': self._result
+            'exception': self._exception_type.__name__
             }
+
+    def get_result(self):
+        return self._result
+
+    def is_exception(self, exception_type):
+        return self._exception_type is exception_type
 
 
 class Error(RuntimeError):
-    def __init__(self, status: int, descr: str):
-        self.response = Response(status, descr)
+    def __init__(self, type, descr: str):
+        self.response = Result(descr, None, type)
 
 
 class MethodIsNotSpecifiedError(Error):
     def __init__(self):
-        super().__init__(1, 'method is not specified')
+        super().__init__(type(self), 'method is not specified')
 
 
 class MethodArgumentsAreNotSpecifiedError(Error):
     def __init__(self):
-        super().__init__(2, 'method arguments (`args list`) are not specified')
+        super().__init__(type(self), 'method arguments (`args list`) are not specified')
 
 
 class NoSuchMethodError(Error):
     def __init__(self, input_method_name):
-        super().__init__(3, 'no method `%s` exists' % input_method_name)
-    
+        super().__init__(type(self), 'no method `%s` exists' % input_method_name)
+
+
+class RequiredArgumentIsMissedError(Error):
+    def __init__(self, arg_name: str):
+        super().__init__(type(self), 'required argument `%s` is missed' % arg_name)
+
+
+class InvalidArgumentTypeError(Error):
+    def __init__(self, name: str, received: str, expected: str):
+        super().__init__(type(self), 'argument `%s` has invalid type `%s`, `%s` expected' % (name, received, expected))
--- test.py
+++ test.py
@@ -1,15 +1,16 @@
 import json
 import unittest
 
-from . import jsonapi
+import jsonapi
 
 
 class JsonAPITest(unittest.TestCase):
 
-    def test_general(self):
-        specification = jsonapi.Specification()
-        specification.add_method(self.simple_method)
+    def setUp(self):
+        self.specification = jsonapi.Specification()
+        self.specification.add_method(self.simple_method)
 
+    def test_general(self):
         json_str = '''{
             "method" : "simple_method",
             "args" : {
@@ -19,7 +20,21 @@ class JsonAPITest(unittest.TestCase):
         }
         '''
         json_obj = json.loads(json_str)
+        response = self.specification.invoke_method(json_obj)
+        self.assertEqual(response.get_result(), 3)
 
+    def test_arguments_types_validation(self):
+        json_str = '''{
+            "method" : "simple_method",
+            "args" : {
+                "a" : "1",
+                "b" : 2
+            }
+        }
+        '''
+        json_obj = json.loads(json_str)
+        response = self.specification.invoke_method(json_obj)
+        self.assertTrue(response.is_exception(jsonapi.InvalidArgumentTypeError))
 
     def simple_method(self, a: int, b: int) -> int:
         return a + b
